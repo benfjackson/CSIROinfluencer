4cdf0c678fdecdca579ac78a1f1079d67de8d309
SirLoddington <benfischerj@gmail.com>
2025-06-10 14:24:39 +1000

    prints

--- orchestration.py
+++ orchestration.py
@@ -3,9 +3,13 @@ from layers.processing import process
 from layers.contentGeneration import generate_images
 
 def run_pipeline():
+    print("Starting pipeline execution...")
     ingest()
+    print("Ingestion completed. Processing articles...")
     process()
+    print("Processing completed. Generating images...")
     generate_images()
+    
 
 if __name__ == "__main__":
     run_pipeline()
